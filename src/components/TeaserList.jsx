1a6cdbef04e3d6b5c39af704c18e0dd7a9107db9
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-02 09:55:10 +0200

    refactoring code

--- src/components/TeaserList.jsx
+++ src/components/TeaserList.jsx
@@ -1,44 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import reports from "../modules/reports";
-
-// const TeaserList = () => {
-//   let [crimeData, setCrimeData] = useState([]);
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const data = await reports.fetchTeaserList();
-//       setCrimeData(data);
-//     };
-//     fetchData();
-//   }, []);
-
-//   const Toggle = () => {
-//     let [toggle, setToggle] = useState(!Toggle);
-//     useEffect(() => {
-//       setToggle();
-//     })
-//   }
-
-//   let teaserList = crimeData.map((data) => {
-//     return (
-//       <div data-id={data.id} data-cy={"data-" + data.id} key={data.id}>
-       
-//        <button> <h3 data-cy="teaser">{data.description}</h3>     </button>
-       
-//       <p data-cy="content" >{data.content}</p>
-//       </div>
-//     );
-//   });
-
-//   return (
-//     <>
-//       <h1>Crime Reports</h1>
-//       {teaserList}
-//     </>
-//   );
-// };
-
-// export default TeaserList;
-
 import React, { Component } from "react";
 import { fetchTeaserListData } from "../modules/reports";
 
@@ -53,30 +12,24 @@ class ReportList extends Component {
     this.setState({ data: data });
   };
 
- 
-
   render() {
-    let teaserList;
-    if (this.state.data !== []) {
-      teaserList = this.state.data.map((data) => {
-        return (
-          <div data-id={data.id} data-cy={"data-" + data.id} key={data.id}>
-         <button onClick={() => this.setState({isHidden: true})}> <h3 data-cy="teaser">{data.description}</h3>     </button>
-       {this.state.isHidden && (
-          <p data-cy="content" >{data.content}</p>
-       )}
-      
-          </div>
-        );
-      });
-      return ( 
-        <div>
-             <h1>Crime Reports</h1>
-       {teaserList}
-       </div>
-     );
-   }
+    let teaserList = this.state.data.map((data) => {
+      return (
+        <div data-cy={"data-" + data.id} key={data.id}>
+          <button onClick={() => this.setState({ isHidden: true })}>
+            <h3 data-cy="teaser">{data.description}</h3>{" "}
+          </button>
+          {this.state.isHidden && <p data-cy="content">{data.content}</p>}
+        </div>
+      );
+    });
+    return (
+      <>
+        <h1>Crime Reports</h1>
+        {teaserList}
+      </>
+    );
   }
 }
-  
-   export default ReportList
+
+export default ReportList;
