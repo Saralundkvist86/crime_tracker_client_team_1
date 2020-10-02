dab2bc15ca550fcec1ccb63d914317ef2603e8c7
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-02 10:29:34 +0200

    formating TeaserList.jsx with hooks

--- src/components/TeaserList.jsx
+++ src/components/TeaserList.jsx
@@ -1,35 +1,71 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { fetchTeaserListData } from "../modules/reports";
+
+// class ReportList extends Component {
+//   state = {
+//     data: [],
+//     isHidden: false,
+//   };
+
+//   componentDidMount = async () => {
+//     const data = await fetchTeaserListData();
+//     this.setState({ data: data });
+//   };
+
+//   render() {
+//     let teaserList = this.state.data.map((data) => {
+//       return (
+//         <div data-cy={"data-" + data.id} key={data.id}>
+//           <button onClick={() => this.setState({ isHidden: true })}>
+//             <h3 data-cy="teaser">{data.description}</h3>{" "}
+//           </button>
+//           {this.state.isHidden && <p data-cy="content">{data.content}</p>}
+//         </div>
+//       );
+//     });
+//     return (
+//       <>
+//         <h1>Crime Reports</h1>
+//         {teaserList}
+//       </>
+//     );
+//   }
+// }
+
+// export default ReportList;
+import React, { useState, useEffect } from "react";
 import { fetchTeaserListData } from "../modules/reports";
 
-class ReportList extends Component {
-  state = {
-    data: [],
-    isHidden: false,
-  };
+const TeaserList = () => {
+  let [crimeData, setCrimeData] = useState([]);
+  let [isHidden, toggleIsHidden] = useState(false);
+  // debugger
 
-  componentDidMount = async () => {
-    const data = await fetchTeaserListData();
-    this.setState({ data: data });
-  };
-
-  render() {
-    let teaserList = this.state.data.map((data) => {
-      return (
-        <div data-cy={"data-" + data.id} key={data.id}>
-          <button onClick={() => this.setState({ isHidden: true })}>
-            <h3 data-cy="teaser">{data.description}</h3>{" "}
-          </button>
-          {this.state.isHidden && <p data-cy="content">{data.content}</p>}
-        </div>
-      );
-    });
+  useEffect(() => { 
+    const fetchTeasersData = async () => {
+      const data = await fetchTeaserListData();
+      setCrimeData(data);
+    } 
+    fetchTeasersData()
+  }, []);
+  let teaserList = crimeData.map((data) => {
     return (
-      <>
-        <h1>Crime Reports</h1>
-        {teaserList}
-      </>
+      <div data-cy={"data-" + data.id} key={data.id}>
+        <button onClick={() => toggleIsHidden(true)}>
+          <h3 data-cy="teaser">{data.description}</h3>{" "}
+        </button>
+        {isHidden && <p data-cy="content">{data.content}</p>}
+      </div>
     );
-  }
-}
+  });
 
-export default ReportList;
+  return (
+    <>
+      <h1>Crime Reports</h1>
+
+      {teaserList}
+    </>
+  );
+};
+
+export default TeaserList;
